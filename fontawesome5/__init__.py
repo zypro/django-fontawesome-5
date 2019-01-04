8081beb91729840e5162e266199370d48bd4133e
Benjamin Frost <info@btfrost.com>
2019-01-04 10:56:31 -0400

    Moved icon redering logic from template tag to Icon class

--- fontawesome5/__init__.py
+++ fontawesome5/__init__.py
@@ -1,21 +1,54 @@
 from django.conf import settings
+from django.utils.html import format_html, mark_safe
 from django.utils.html import format_html
 
 class Icon(object):
 
-    def __init__(self, style_prefix, icon_id):
-        self.style_prefix = style_prefix
-        self.icon_id = icon_id
-
     def as_html(self):
-        if not self.icon_id:
+        if not self.icon and self.style_prefix:
             return ''
+        base_string = '<i {title} class="{style_prefix} {prefix}-{icon}{border}{fixed_width}{flip}{li}{pull}{pulse}{rotate}{size}{spin}" {color}></i>'
+        return format_html(base_string.format(
+            style_prefix=self.style_prefix,
+            icon=self.icon,
+            prefix=self.prefix,
 
-        prefix = getattr(settings, 'FONTAWESOME_PREFIX', 'fa')
-        return format_html('<i class="{0} {1}-{2}"></i>', self.style_prefix, prefix, self.icon_id)
+            title=' title={}'.format(self.title) if self.title else '',
+            color='style="color:{};"'.format(self.color) if self.color else '',
+
+            border=' fa-border' if self.border else '',
+            fixed_width=' fa-fw' if self.fixed_width else '',
+            flip=' fa-flip-{}'.format(self.flip) if self.flip else '',
+            li=' fa-li' if self.li else '',
+            pull=' fa-pull-{}'.format(self.pull) if self.pull else '',
+            pulse=' fa-pulse' if self.pulse else '',
+            rotate=' fa-rotate-{}'.format(self.rotate) if self.rotate else '',
+            size=' {}'.format(self.size) if self.size else '',
+            spin=' fa-spin' if self.spin else '',
+        ))
+
+    def __init__(self, style_prefix, icon, title='', color='',
+        border=False, fixed_width=False, flip='', li=False, pull='', pulse=False, rotate=0, size='', spin=False):
+
+        self.style_prefix = style_prefix
+        self.prefix = getattr(settings, 'FONTAWESOME_PREFIX', 'fa')
+        self.icon = icon
+        self.title = title
+        self.color = color
+        self.border = border
+        self.fixed_width = fixed_width
+        self.flip = flip
+        self.li = li
+        self.pull = pull
+        self.pulse = pulse
+        self.rotate = rotate
+        self.size = size
+        self.spin = spin
 
     def __str__(self):
         return "['{}', '{}']".format(self.style_prefix, self.icon_id)
 
     def __unicode__(self):
         return str(self)
+
+        
--- fontawesome5/templatetags/fontawesome.py
+++ fontawesome5/templatetags/fontawesome.py
@@ -5,32 +5,14 @@ from django.conf import settings
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.utils.html import format_html, mark_safe
 
+from .. import Icon
+
 register = template.Library()
 
 
 @register.simple_tag
-def fa5_icon(
-    style_prefix, icon, color=False, title='',
-    border=False, fixed_width=False, flip=False, li=False, pull=False, pulse=False, rotate=False, size=False, spin=False):
-
-    return mark_safe('<i {title} class="{style_prefix} {prefix}-{icon}{border}{fixed_width}{flip}{li}{pull}{pulse}{rotate}{size}{spin}" {color}></i>'.format(
-        style_prefix=style_prefix,
-        icon=icon,
-        prefix=getattr(settings, 'FONTAWESOME_PREFIX', 'fa'),
-
-        color='style="color:{};"'.format(color) if color else '',
-        title=' title={}'.format(title) if title else '',
-
-        border=' fa-border' if border else '',
-        fixed_width=' fa-fw' if fixed_width else '',
-        flip=' fa-flip-{}'.format(flip) if flip else '',
-        li=' fa-li' if li else '',
-        pull=' fa-pull-{}'.format(pull) if pull else '',
-        pulse=' fa-pulse' if pulse else '',
-        rotate=' fa-rotate-{}'.format(rotate) if rotate else '',
-        size=' {}'.format(size) if size else '',
-        spin=' fa-spin' if spin else '',
-    ))
+def fa5_icon(*args, **kwargs):
+    return Icon(*args, **kwargs).as_html()
 
 
 @register.simple_tag
